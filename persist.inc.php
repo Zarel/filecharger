939b9544347eb2d9cf696689db5fbfee1c2231f9
Guangcong Luo <guangcongluo@gmail.com>
2014-02-14 08:21:01 -0800

    Cleanup
    persist.inc.php should not start with inst_id set, and there's no need
    to have temp.bin appear in cache/

--- persist.inc.php
+++ persist.inc.php
@@ -19,11 +19,7 @@ $_PERSIST = array (
       'pass' => false,
       'psub' => '',
       'priv' => 127,
-      'sid' => '2da19b76572812e2adb322c5f2afd5ff',
-      'pli' => 1253508753,
-      'rem' => true,
-    )
+      'sid' => false,
+    ),
   ),
-  'inst_id' => '0c31596c3a8efaae727bdb4279d8b36d',
 );
-?>
